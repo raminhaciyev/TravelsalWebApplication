1e59528165d84c1349fadfdb47e228f6e4139a26
Ramin <ramin.haciyev.2014@gmail.com>
2023-09-11 19:20:36 +0200

    Register and Login templates are implemented, and the register process is successfully completed.

---
+++ TravelsalWebApplication/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelsalWebApplication.Models;
+
+namespace TravelsalWebApplication.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+        {
+            AppUser app = new AppUser()
+            {
+                Name = p.Name,
+                Surname = p.Surname,
+                Email = p.Mail,
+                PhoneNumber=p.Phone
+            };
+
+            if (p.Password==p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(app, p.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelsalWebApplication/Models/UserRegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelsalWebApplication.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Please fill your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please fill your Surname")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Please fill your mail address")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Please fill your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please fill your password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please repeat your password")]
+        [Compare("Password",ErrorMessage ="Passwords are not matched")]
+        public string ConfirmPassword { get; set; }
+    }
+}

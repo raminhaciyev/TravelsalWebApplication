8f214a7f337a8ba3e290c03be94401a5f18472bb
Ramin <ramin.haciyev.2014@gmail.com>
2023-09-15 21:07:44 +0200

    Booking the new reservation by user who authenticated to the system

---
+++ TravelsalWebApplication/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelsalWebApplication.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservaion()
+        {
+            List<SelectListItem> values = (from x in destinationManager.TListGet()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservaion(Reservation p)
+        {
+            p.AppUserId = 1;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
+        }
+    }
+}

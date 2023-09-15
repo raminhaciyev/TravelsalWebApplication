75070b2e85a6fe549665eee53ae1dff2edceca35
Ramin <ramin.haciyev.2014@gmail.com>
2023-09-15 18:37:07 +0200

    Getting the user information who authonticated to the system

---
+++ TravelsalWebApplication/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelsalWebApplication.Areas.Member.Models
+{
+    public class UserEditViewModel
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string password { get; set; }
+        public string confirmpassword { get; set; }
+        public string phone { get; set; }
+        public string mail { get; set; }
+        public string gender { get; set; }
+        public string imageurl { get; set; }
+    }
+}

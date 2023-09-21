f76b6c61f2c3ae3382c901402b5d67de5390187e
Ramin <ramin.haciyev.2014@gmail.com>
2023-09-21 22:47:33 +0200

    CRUD operations in Admin Area, Partial views

--- TravelsalWebApplication/Areas/Admin/Controllers/DestinationController.cs
+++ TravelsalWebApplication/Areas/Admin/Controllers/DestinationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,50 @@ using System.Threading.Tasks;
 namespace TravelsalWebApplication.Areas.Admin.Controllers
 {
     [Area("Admin")]
+ 
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
+            var values = destinationManager.TListGet();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination p)
+        {
+           destinationManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index","Destination", new { area="Admin"});
+        }
+
+       [HttpGet]
+       public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            return View(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination p)
+        {
+            destinationManager.TUpdate(p);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
         }
     }
 }
---
+++ TravelsalWebApplication/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelsalWebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialMenu()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}

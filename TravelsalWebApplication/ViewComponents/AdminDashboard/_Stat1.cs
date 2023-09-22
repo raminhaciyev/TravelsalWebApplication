140c2e927de4ef8f96d39137e8ad328cdd3f2b0b
Ramin <ramin.haciyev.2014@gmail.com>
2023-09-23 00:39:57 +0200

    Admin Dashboard is divided to view components.

---
+++ TravelsalWebApplication/ViewComponents/AdminDashboard/_Stat1.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelsalWebApplication.ViewComponents.AdminDashboard
+{
+    public class _Stat1:ViewComponent
+    {
+        Context x = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = x.Destinations.Count();
+            ViewBag.v2 = x.Users.Count();
+            return View();
+        }
+    }
+}
---
+++ TravelsalWebApplication/ViewComponents/AdminDashboard/_Tab3.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace TravelsalWebApplication.ViewComponents.AdminDashboard
+{
+    public class _Tab3 : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

39836e7cead990641bea97548b02fe00657904fc
Ramin <ramin.haciyev.2014@gmail.com>
2023-10-07 13:51:39 +0200

    Exchange Rates From Rapid Api

---
+++ TravelsalWebApplication/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using TravelsalWebApplication.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace TravelsalWebApplication.Areas.Admin.Controllers
+{
+    public class ApiExchangeController : Controller
+    {
+        [Area("Admin")]
+
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel> bookingExchangeViewModels = new List<BookingExchangeViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apidojo-booking-v1.p.rapidapi.com/currency/get-exchange-rates?base_currency=USD&languagecode=en-us"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "3e8f7313a2mshab21d49cf386f22p1febeejsn3b6fc07d5d9d" },
+        { "X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel>(body);
+                return View(values.exchange_rates);
+            }
+        }
+    }
+}

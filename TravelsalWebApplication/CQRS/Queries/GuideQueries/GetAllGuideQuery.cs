f492202f1bd85646a02f03d9201601981a70f727
Ramin <ramin.haciyev.2014@gmail.com>
2023-10-14 12:43:00 +0200

    MediatR Library

---
+++ TravelsalWebApplication/CQRS/Queries/GuideQueries/GetAllGuideQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelsalWebApplication.CQRS.Resuls.GuideResults;
+
+namespace TravelsalWebApplication.CQRS.Queries.GuideQueries
+{
+    public class GetAllGuideQuery : IRequest<List<GetAllGuideQueryResult>>
+    {
+    }
+}
